85d1fa886541f742aa55417b367dcfa29328d314
habib33-3 <habib.rahman0330@gmail.com>
2026-04-09 14:43:42 +0600

    check log

--- components/google-login-button.tsx
+++ components/google-login-button.tsx
@@ -25,7 +25,11 @@ export function GoogleLoginButton({ returnTo, redirect }: Props) {
   }, []);
 
   const handleGoogleLogin = async () => {
+console.log(process.env.BACKEND_API_URL, process.env.NEXT_PUBLIC_BACKEND_API_URL)
     const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+
+    console.log("🚀 ~ google-login-button.tsx:31 ~ handleGoogleLogin ~ clientId:", clientId)
+
     if (!clientId) {
       setError("Google client ID not configured");
       return;
