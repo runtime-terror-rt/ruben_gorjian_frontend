39e141951fccc7fd3f8b149bdfc7950ab1965e10
theashim-3 <ashim@softvence.com>
2026-07-14 15:03:54 +0600

    updated the landing page

--- components/newhome/Navbar.tsx
+++ components/newhome/Navbar.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Navbar() {
   return (
     <>
-<nav className="nav">
-  <a href="#" className="nav-brand">Talexia<span className="dot">.</span>us</a>
+<nav className="nav items-center justify-between">
+  <a href="#" className="nav-brand">Talexia</a>
   <div className="nav-links">
     <a href="#work">Work</a>
     <a href="#process">Process</a>
